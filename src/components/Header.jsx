1c66ba32b890babbd40f47233aae82e33b6daec5
mahima98 <mahimaramgolam98@gmail.com>
2023-01-23 16:26:45 +0400

    Header wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import Logo from "../assets/img/logo.png";
+import Nav from "../components/Nav";
+import NavMobile from "../components/NavMobile";
+
+import { FaBars } from "react-icons/fa";
+import { BsArrowRight } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container mx-auto">
+        <div className="">
+          <a href="#">
+            <img src={Logo} alt="" />
+          </a>
+
+          <div className="hidden lg:flex">
+            <Nav />
+          </div>
+
+          <div className="xl:hidden">
+            <NavMobile />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
