3366358c4f99eee5d32f68ad8291ec90e4b9dff8
mahima98 <mahimaramgolam98@gmail.com>
2023-01-23 14:04:43 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+import "./App.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
